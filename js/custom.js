4e338c30d4fc28176b9e5993e0b27cc05ec8c092
kangaro0 <johannes_herbert@yahoo.de>
2017-09-15 16:37:20 +0200

    update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
 
 Reveal.addEventListener( 'fragmentshown', ( event ) => {
     let element = event.fragment;
+
+    if( element.classList.contains( 'shake' ) ){
+        element.classList.add('shake-it');
+        
+        setTimeout( function(){
+            element.classList.remove( 'shake-it' )
+        }, 820 );
+    }
 });
+
